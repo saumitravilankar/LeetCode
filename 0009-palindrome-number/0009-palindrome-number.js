411399151ebd135225c385bb69b1db96216ee68c
Saumitra Vilankar <saumitra.vilankar77@gmail.com>
2024-08-20 20:39:18 +0530

    Time: 99 ms (99.52%), Space: 58.5 MB (27.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,8 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    console.log(Array.from(x.toString()).join(''))
-    console.log(Array.from(x.toString()).reverse().join(''))
     if (Array.from(x.toString()).join('') === Array.from(x.toString()).reverse().join('')) {
         return true
     }
