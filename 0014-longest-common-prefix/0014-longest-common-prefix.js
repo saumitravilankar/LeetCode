7a37ef52512368654aa7cc7def0bfd06c8eba132
Saumitra Vilankar <saumitra.vilankar77@gmail.com>
2024-08-25 00:54:19 +0530

    Time: 233 ms (5.01%), Space: 57.8 MB (5.05%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    // if (strs.length === 1) {
+    //     return strs[0]
+    // }
+
+    const sortedArr = strs.sort((a, b) => a.length - b.length)
+    console.log(sortedArr)
+    let result = ""
+    for (let i = 0; i < sortedArr[0].length; i++) {
+        const letter = sortedArr[0][i];
+        let flag = false;
+        console.log(flag, '------flag1')
+        for (let j = 0; j < sortedArr.length; j++) {
+            console.log(sortedArr[j][i], '------------to compare')
+            if (letter === sortedArr[j][i]) {
+                flag = true;
+            } else {
+                flag = false;
+                break;
+            }
+            console.log(flag, '------flag2')
+        }
+        if (flag === true) {
+            result += letter
+            console.log(flag, '------flag3')
+            console.log(letter, '------letter')
+        } else {
+            break;
+        }
+    }
+    return result
+};
